4e3df06ff23ca50c3c4e8d02d67ed6cd2a1b658c
Otoniel <rmunoz1803@alumno.ipn.mx>
2026-06-14 16:10:37 -0600

    changin port

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/api/reservas', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
